94f8527b632a1970cc4d1c304d519b5e9d79148f
WitcherHawke <coloneljihad@gmail.com>
2022-04-14 15:06:35 -0600

    stuff

---
+++ ARAppTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ARAppTest
+//
+//  Created by Brittany May on 4/7/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+    
+   
+    
+}
