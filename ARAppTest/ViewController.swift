55b2acf10af73f4730db2ab3a91013db76d3f138
AlexandraChavez <70164908+AlexandraChavez@users.noreply.github.com>
2022-04-14 15:59:11 -0600

    gradient

--- ARAppTest/ViewController.swift
+++ ARAppTest/ViewController.swift
@@ -27,6 +27,15 @@ class ViewController: UIViewController {
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        // Background Gradients
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            Color.init(<#T##colorSpace: Color.RGBColorSpace##Color.RGBColorSpace#>, red: 42, green: 54, blue: 69, opacity: 1),
+            Color.init(<#T##colorSpace: Color.RGBColorSpace##Color.RGBColorSpace#>, red: 59, green: 82, blue: 126, opacity: 1)
+        ]
+        view.layer.addSublayer(gradientLayer)
     }
     
     //class ARImageAnchor: ARAnchor{
@@ -34,6 +43,8 @@ class ViewController: UIViewController {
     //}
     
     
+    
+    
     }
     
-}
+
