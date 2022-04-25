0b3b7ef4548cfa189e8e2933c74ad98ac3d088fd
WitcherHawke <coloneljihad@gmail.com>
2022-04-25 12:50:12 -0600

    Stuff

--- jamAppTest/jamAppTest/ViewController.swift
+++ jamAppTest/jamAppTest/ViewController.swift
@@ -13,6 +13,7 @@ import ARKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    var testingScene = Experience.TestScene()
     
     @IBAction func startButton(_ sender: Any) {
         
@@ -35,14 +36,19 @@ class ViewController: UIViewController {
               //  )
             //}
        // }
-        
-        
+        var body: some View {
+        ZStack {
+            Image ("bg")
+            
+            }
+        }
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadTestScene()
+        let arAnchor = try! Experience.loadTestScene()
+     
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(arAnchor)
         
         
     }
