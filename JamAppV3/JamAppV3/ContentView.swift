121cd8543beef6e71ea6bc9d439eb2ec153a71b2
WitcherHawke <62607565+WitcherHawke@users.noreply.github.com>
2022-05-04 13:38:19 -0600

    AR

--- JamAppV3/JamAppV3/ContentView.swift
+++ JamAppV3/JamAppV3/ContentView.swift
@@ -38,7 +38,29 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
+    
+    
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+    
+    struct ARViewContainer2: UIViewRepresentable {
+        
+        func makeUIView(context: Context) -> ARView {
+            
+            let arView = ARView(frame: .zero)
+            
+            // Load the "Box" scene from the "Experience" Reality File
+            let boxAnchor = try! Experience.loadBox()
+            
+            // Add the box anchor to the scene
+            arView.scene.anchors.append(boxAnchor)
+            
+            return arView
+            
+        }
+        
+        func updateUIView(_ uiView: ARView, context: Context) {}
     
 }
 
