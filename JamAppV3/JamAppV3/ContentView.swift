e1befd1775c2f3ead6049bbb6956600917ad9cbb
WitcherHawke <62607565+WitcherHawke@users.noreply.github.com>
2022-05-04 13:49:14 -0600

    maybe not break

--- JamAppV3/JamAppV3/ContentView.swift
+++ JamAppV3/JamAppV3/ContentView.swift
@@ -51,10 +51,10 @@ struct ARViewContainer: UIViewRepresentable {
             let arView = ARView(frame: .zero)
             
             // Load the "Box" scene from the "Experience" Reality File
-            let boxAnchor = try! Experience.loadBox()
+            let jeanne = try! Experience.loadJeanneDesk()
             
             // Add the box anchor to the scene
-            arView.scene.anchors.append(boxAnchor)
+            arView.scene.anchors.append(jeanne)
             
             return arView
             
